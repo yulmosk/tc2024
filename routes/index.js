60ec2c918ae899a813c477a756b81fba5b75dfdd
Юлия Москалева <yulia@MacBook-Pro-Ulia.local>
2024-10-02 08:51:00 +0300

    tc_4_2

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,20 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* Страница Коржика*/
+router.get('/korzhik', function(req, res, next) {
+  res.send("<h1>Страница Коржика</h1>")
+});
+
+/* Страница Карамельки */
+router.get('/karamelka', function(req, res, next) {
+  res.send("<h1>Страница Карамельки</h1>")
+});
+
+/* Страница Компота */
+router.get('/kompot', function(req, res, next) {
+  res.send("<h1>Страница Компота</h1>")
+});
+
+
 module.exports = router;

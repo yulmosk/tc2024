8eab3e5b33daf24339dca935603d8341fb31f04a
Юлия Москалева <yulmosk@mail.ru>
2024-10-15 23:41:14 +0300

    tc_6_2

--- createDB.js
+++ createDB.js
@@ -1,6 +1,9 @@
 const { MongoClient } = require('mongodb');
 // or as an es module:
 // import { MongoClient } from 'mongodb'
+var data = require("./data.js").data;
+
+
 
 // Connection URL
 const url = 'mongodb://localhost:27017';

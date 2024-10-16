d9ec0aae95a576cae1d0b48e2c6644ba001f5fca
Юлия Москалева <yulmosk@mail.ru>
2024-10-16 20:16:22 +0300

    tc_10_2

--- routes/index.js
+++ routes/index.js
@@ -11,6 +11,16 @@ router.get('/', function(req, res, next) {
 router.get('/logreg', function(req, res, next) {
   res.render('logreg',{title: 'Вход'});
   });
+
+
+  /* POST login/registration page. */
+router.post('/logreg', function(req, res, next) {
+   var username = req.body.username
+   var password = req.body.password
+   console.log(username);
+   console.log(password);
+});
+
     
 
 

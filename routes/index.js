433f873666671fe3195bb8dabc277512691453d3
Юлия Москалева <yulmosk@mail.ru>
2024-10-16 19:33:18 +0300

    tc_10_1

--- routes/index.js
+++ routes/index.js
@@ -7,5 +7,11 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express',  counter:req.session.counter  });
 });
 
+/* GET login/registration page. */
+router.get('/logreg', function(req, res, next) {
+  res.render('logreg',{title: 'Вход'});
+  });
+    
+
 
 module.exports = router;

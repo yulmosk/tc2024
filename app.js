cfabef4fe3c7e8ec5235a7412d58953d5a82c92d
Юлия Москалева <yulmosk@mail.ru>
2024-10-16 18:02:48 +0300

    tc_9_1

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/tc2024');
+var session = require("express-session")
 
 
 var indexRouter = require('./routes/index');
@@ -24,6 +25,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session({
+  secret: "ThreeCats",
+  cookie:{maxAge:60*1000},
+  proxy: true,
+  resave: true,
+  saveUninitialized: true
+}))
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);

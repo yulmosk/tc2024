9e01eece1ea1e591f51438c1a09df49dcc6388e8
Юлия Москалева <yulmosk@mail.ru>
2024-10-16 12:07:36 +0300

    tc_7_3

---
+++ models/cat.js
@@ -0,0 +1,20 @@
+var mongoose = require('mongoose')
+var Schema = mongoose.Schema
+
+
+
+var catSchema = new Schema({
+    title: String,
+    nick: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    avatar: String,
+    desc: String,
+    created:{
+        type:Date,
+        default:Date.now
+    }
+})
+module.exports.Cat = mongoose.model("Cat", catSchema)
--- testMongoose.js
+++ testMongoose.js
@@ -1,18 +1,13 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://127.0.0.1:27017/testMongoose2024');
 
-var schema = mongoose.Schema({ name: String })
+var Cat = require('./models/cat.js').Cat
+
+var cat = new Cat({
+    title: "Коржик",
+    nick: "korzhik",
+})
 
 
-schema.methods.meow = function(){
-    console.log(this.name + " сказал мяу")
-}
-
-
-const Cat = mongoose.model('Cat', schema);
-
-
-
-const kitty = new Cat({ name: 'Пушок' });
-kitty.save().then(() => kitty.meow());
+cat.save();
 

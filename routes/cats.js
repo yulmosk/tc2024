a839f6496a5d0ed6fc792a8b3edf82e5dba136ea
Юлия Москалева <yulmosk@mail.ru>
2024-10-16 17:14:37 +0300

    tc_8_3

--- routes/cats.js
+++ routes/cats.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var Cat = require('../models/cat').Cat;
 
 /* GET cats listing. */
 router.get('/', function(req, res, next) {
@@ -7,8 +8,16 @@ router.get('/', function(req, res, next) {
 });
 
 /* Страница котов */
-router.get("/:nick", function(req, res, next) {
-    res.send(req.params.nick);
+router.get("/:nick", async function(req, res, next) {
+    var cats = await Cat.find({nick: req.params.nick});
+    console.log(cats)
+    if(!cats.length) return next(new Error("Нет такого котенка в мультфильме Три кота"))
+        var cat = cats[0];
+        res.render('cat', {
+            title: cat.title,
+            picture: cat.avatar,
+            desc: cat.desc
+        })
 });
 
 
